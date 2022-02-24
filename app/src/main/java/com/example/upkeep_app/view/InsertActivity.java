f97fa9e3cc2d228a3154c3020f7f86382d081822
Brais <braisfv22@gmail.com>
2022-02-24 22:19:19 +0100

    all insert entities

---
+++ app/src/main/java/com/example/upkeep_app/view/InsertActivity.java
@@ -0,0 +1,78 @@
+package com.example.upkeep_app.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.upkeep_app.R;
+
+public class InsertActivity extends AppCompatActivity {
+
+    public static final String EXTRA_FLEET = "FLEET";
+    public static final String EXTRA_BOAT = "BOAT";
+    public static final String EXTRA_SERVICE = "SERVICE";
+    public static final String EXTRA_COMPONENT = "COMPONENT";
+    public static final String EXTRA_UPKEEP = "UPKEEP";
+    public static final String EXTRA_TASK = "TASK";
+    public static final String EXTRA_OPERATOR = "OPERATOR";
+    public static final String EXTRA_STORE = "STORE";
+    private EditText eTFleet, eTBoat, eTService, eTComponent, eTUpkeep, eTTask, eTOperator, eTStore;
+    private Button bIFleet, bIBoat, bIService, bIComponent, bIUpkeep, bITask, bIOperator, bIStore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_fleet);
+        // EditTexts
+        eTFleet = findViewById(R.id.eTFleets);
+        eTBoat = findViewById(R.id.eTBoats);
+        eTService = findViewById(R.id.eTServices);
+        eTComponent = findViewById(R.id.eTComponents);
+        eTUpkeep = findViewById(R.id.eTUpkeeps);
+        eTTask = findViewById(R.id.eTTasks);
+        eTOperator = findViewById(R.id.eTOperators);
+        eTStore = findViewById(R.id.eTStores);
+        // Buttons
+        bIFleet = findViewById(R.id.bInsertFleet);
+        setButtonListener(bIFleet, eTFleet, EXTRA_FLEET);
+
+        bIBoat = findViewById(R.id.bInsertBoat);
+        setButtonListener(bIBoat, eTBoat, EXTRA_BOAT);
+
+        bIService = findViewById(R.id.bInsertService);
+        setButtonListener(bIService, eTService, EXTRA_SERVICE);
+
+        bIComponent = findViewById(R.id.bInsertComponent);
+        setButtonListener(bIComponent, eTComponent, EXTRA_COMPONENT);
+
+        bIUpkeep = findViewById(R.id.bInsertUpkeep);
+        setButtonListener(bIUpkeep, eTUpkeep, EXTRA_UPKEEP);
+
+        bITask = findViewById(R.id.bInsertTask);
+        setButtonListener(bITask, eTTask, EXTRA_TASK);
+
+        bIOperator = findViewById(R.id.bInsertOperator);
+        setButtonListener(bIOperator, eTOperator, EXTRA_OPERATOR);
+
+        bIStore = findViewById(R.id.bInsertStore);
+        setButtonListener(bIStore, eTStore, EXTRA_STORE);
+    }
+
+    private void setButtonListener(Button bt, EditText et, String extra) {
+        bt.setOnClickListener(view -> {
+            Intent replyIntent = new Intent();
+            if (TextUtils.isEmpty(et.getText())) {
+                setResult(RESULT_CANCELED, replyIntent);
+            } else {
+                String word = et.getText().toString();
+                replyIntent.putExtra(extra, word);
+                setResult(RESULT_OK, replyIntent);
+            }
+            finish();
+        });
+    }
+}
