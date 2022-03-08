a78372808fb3c1ecfe46dc8a7c41be8302ee4a1e
Brais <braisfv22@gmail.com>
2022-03-08 21:01:59 +0100

    moved extras to a Class

---
+++ app/src/main/java/com/example/upkeep_app/util/Extras.java
@@ -0,0 +1,12 @@
+package com.example.upkeep_app.util;
+
+public class Extras {
+    public static final String EXTRA_FLEET = "FLEET";
+    public static final String EXTRA_BOAT = "BOAT";
+    public static final String EXTRA_SERVICE = "SERVICE";
+    public static final String EXTRA_COMPONENT = "COMPONENT";
+    public static final String EXTRA_UPKEEP = "UPKEEP";
+    public static final String EXTRA_TASK = "TASK";
+    public static final String EXTRA_OPERATOR = "OPERATOR";
+    public static final String EXTRA_STORE = "STORE";
+}
