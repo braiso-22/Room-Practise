6ded3a28dc5e19a0f160d7bea5bfd461d6600d39
Brais <braisfv22@gmail.com>
2022-03-08 21:02:33 +0100

    refactor

--- app/src/main/java/com/example/upkeep_app/view/InsertActivity.java
+++ app/src/main/java/com/example/upkeep_app/view/InsertActivity.java
@@ -1,5 +1,7 @@
 package com.example.upkeep_app.view;
 
+import static com.example.upkeep_app.util.Extras.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,14 +14,8 @@ import com.example.upkeep_app.R;
 
 public class InsertActivity extends AppCompatActivity {
 
-    public static final String EXTRA_FLEET = "FLEET";
-    public static final String EXTRA_BOAT = "BOAT";
-    public static final String EXTRA_SERVICE = "SERVICE";
-    public static final String EXTRA_COMPONENT = "COMPONENT";
-    public static final String EXTRA_UPKEEP = "UPKEEP";
-    public static final String EXTRA_TASK = "TASK";
-    public static final String EXTRA_OPERATOR = "OPERATOR";
-    public static final String EXTRA_STORE = "STORE";
+
+
     private EditText eTFleet, eTBoat, eTService, eTComponent, eTUpkeep, eTTask, eTOperator, eTStore;
     private Button bIFleet, bIBoat, bIService, bIComponent, bIUpkeep, bITask, bIOperator, bIStore;
 
