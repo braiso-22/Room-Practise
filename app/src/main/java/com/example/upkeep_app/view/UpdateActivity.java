aa5c46d6c4b72ec02b953f159ba154afdb2fca30
Brais <braisfv22@gmail.com>
2022-03-09 20:52:29 +0100

    created UpdateActivity

---
+++ app/src/main/java/com/example/upkeep_app/view/UpdateActivity.java
@@ -0,0 +1,16 @@
+package com.example.upkeep_app.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.upkeep_app.R;
+
+public class UpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+    }
+}
